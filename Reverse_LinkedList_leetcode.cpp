6b4f81a38d044a378157ae69e3ecb38eb8e9fb5a
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-11-06 18:54:40 -0800

    Update Reverse_LinkedList_leetcode.cpp

--- Reverse_LinkedList_leetcode.cpp
+++ Reverse_LinkedList_leetcode.cpp
@@ -8,7 +8,8 @@
 
 // Example 3:  Input: head = []
 //             Output: []
-  
+
+// Iterative solution // 
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -32,6 +33,44 @@ public:
             curr = nextNode;
         }
         head = prev;
+
+// Recursive solution // 
+      /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseUsingRecursion(ListNode* prev, ListNode* curr) {
+        //base case 
+          if(curr == NULL) {
+            return prev;
+        } 
+
+        // 1 case humane solve kr lia 
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+
+        ListNode* recursionAns = reverseUsingRecursion(prev,curr);
+        return recursionAns;
+
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        return reverseUsingRecursion(prev,curr);
+        
+    }
+};
 
     return head;
 
